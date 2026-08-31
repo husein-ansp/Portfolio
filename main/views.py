9607462d14a1500511e39acd32e252567e064df4
Hussein Ansaripour <husein.ansp@gmail.com>
2026-08-31 15:06:59 +0330

    Update header

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+def home(request):
+    return render(request, "home.html")
+def about(request):
+    return render(request, "about.html")
+def skills(request):
+    return render(request, "skills.html")
+def projects(request):
+    return render(request, "projects.html")
+def contact(request):
+    return render(request, "contact.html")
